713a3a4bb655bbffe5a120127e8905e21ccd8986
PentinoGames <pentinogames@gmail.com>
2023-11-21 02:07:03 +0100

    Attack animation fixed and new animations added

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -1,29 +1,38 @@
 extends Area2D
 
-@export var speed = 250 # How fast the player will move (pixels/sec).
+@export var speed = 350 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
 var is_attacking = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
-	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
+	var total_frames_attack =$AnimatedSprite2D.get_sprite_frames().get_frame_count("attack")
 	
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
+		
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
-	if Input.is_action_pressed("attack") or is_attacking:
-		attack()
-	
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		walk(velocity)
-	else:
-		idle()
+		
+	if Input.is_action_pressed("attack"):
+		is_attacking = true
+		$AnimatedSprite2D.play("attack",1.5)
+		
+	if is_attacking:
+		attack(total_frames_attack)
+		
+	if !is_attacking:
+		if velocity.length() > 0 :
+			velocity = velocity.normalized() * speed
+			walk(velocity)
+		else:
+			if velocity.length()==0:
+				idle()
 		
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
@@ -33,10 +42,10 @@ func idle():
 	
 func walk(velocity):
 	$AnimatedSprite2D.flip_v = false
-	# See the note below about boolean assignment.
 	$AnimatedSprite2D.flip_h = velocity.x < 0
 	$AnimatedSprite2D.play("walk")
 	
-func attack():
-	$AnimatedSprite2D.play("attack")
-	is_attacking = true
+func attack(total_frames_attack):
+	if ($AnimatedSprite2D.get_frame()+1) >= total_frames_attack :
+		$AnimatedSprite2D.stop()
+		is_attacking=false
